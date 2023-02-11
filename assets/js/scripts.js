f8571f2a35c2d9f39c213e60babb9c99335791a5
jsakhil <jsakhil70@gmail.com>
2023-02-11 18:55:39 +0530

    Responsive

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -13,7 +13,32 @@ $(document).ready(function(){
 		infinite: true,
 		slidesToShow: 5,
 		slidesToScroll: 5,
-		dots: true
+		dots: true,
+		responsive: [
+			{
+				breakpoint: 992,
+				settings: {
+					slidesToShow: 3,
+					slidesToScroll: 3,
+					infinite: true,
+					dots: true
+				}
+			},
+			{
+				breakpoint: 768,
+				settings: {
+					slidesToShow: 2,
+					slidesToScroll: 2
+				}
+			},
+			{
+				breakpoint: 575,
+				settings: {
+					slidesToShow: 1,
+					slidesToScroll: 1
+				}
+			}
+		]
 	});
 
 
