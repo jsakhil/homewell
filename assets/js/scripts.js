dcaeea6ebd939ef80346056c64bf23f0a63d3a9c
jsakhil <jsakhil70@gmail.com>
2023-02-11 17:00:18 +0530

    5PM Commit

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -4,13 +4,16 @@ $(document).ready(function(){
 	$('.hw_testimonials_slider').slick({
 		infinite: true,
 		slidesToShow: 1,
-		slidesToScroll: 1
+		slidesToScroll: 1,
+		dots: true,
+		arrows: false
 	});
 
 	$('.hw_awards_slider').slick({
 		infinite: true,
 		slidesToShow: 5,
-		slidesToScroll: 1
+		slidesToScroll: 5,
+		dots: true
 	});
 
 
